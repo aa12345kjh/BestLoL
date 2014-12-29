fea3ea860d1c319fd9329dc2dfcfe698c09134ad
Bestian Tang <bestian@gmail.com>
2014-12-29 17:35:26 +0800

    filter init

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
+angular.module("lolApp",[])
+.controller ("myCtrl", myCtrl)
+.filter("hasKey", hasKey);
 
-function myCtrl () {
+function hasKey(){
+  return function(list,k){
+    if (!k) return list;
+    return list.filter(function(o){
+        console.log(o.name + '_' + k);
+        return o.name.indexOf(k) > -1;
+    })
+  }
+}
 
-  this.heros = [
-  {name: '吸血鬼', img:'http://lol.replays.net/db/game/images/page/champions_Vladimir.jpg',text:''},
+function myCtrl ($scope) {
+
+  //models
+  angular.extend($scope, {
+    heros : [
+      {name: '吸血鬼', img:'http://lol.replays.net/db/game/images/page/champions_Vladimir.jpg',text:'這是吸血鬼，他不能被攻擊'},
       {name: '特朗德', img:'http://kiwame-z.com/wp-content/uploads/2014/10/f483afdc8fa3667f273471b2b5223c5d.png',
-text:'<img src= "https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpa1/v/t34.0-12/10833996_875494279169795_222227634_n.jpg?oh=f25307a5f4c81963e53d52e73bab9e29&oe=548D75C1&__gda__=1418493534_20f09bf217c771c625870c0cd68a9d6f">'},
-  {name: '安妮', img:'http://forums.euw.leagueoflegends.com/board/attachment.php?attachmentid=159092&d=1386868587',text:'<img>'},
-  {name: '拉克絲', img:'https://encrypted-tbn3.gstatic.com/images?q=tbn%3AANd9GcSQK2Its9rpYYaTx2Iks5nmz8-Np0V7HCAgwMJrTzQ-JnH0OcGlLw',text:''},
-  {name: '維嘉', img:'https://encrypted-tbn3.gstatic.com/images?q=tbn%3AANd9GcRT6sZ0SUWdnwyLaAVShflhx1RXUjY05vltg06oNSujF-KvKsTp',text:''},
-  {name: '犽宿', img:'http://tecnoslave.com/wp-content/uploads/2013/12/Yasuo_Square_0.jpg',text:''},
-  {name: '菲歐拉', img:'http://i.gbc.tw/2010/zone/lol/champion/120/fiora.png',text:''},
-  {name: '麗珊卓', img:'http://i.gbc.tw/2010/zone/lol/champion/120/lissandra.png',text:''},
-  {name: '阿姆姆', img:'http://i.gbc.tw/2010/zone/lol/champion/120/amumu.png',text:''},
-  {name: '布朗姆', img:'http://img.legames.cn/templates/images/upload/2014/07/1405329974_0.png',text:''},
-  {name: '易大師', img:'http://pic2.52pk.com/files/allimg/140724/2D0S4_203HUE6.png',text:''},
-  {name: '奈德麗', img:'http://tw.loldb.gameguyz.com//images/sqlite_images/champions_avatar_104_104/76.png',text:''},
-  {name: '威寇茲', img:'http://i.gbc.tw/2010/zone/lol/champion/120/velkoz.png',text:''},
-  {name: '雷茲', img:'http://i.gbc.tw/2010/zone/lol/champion/120/ryze.png',text:''},
-  {name: '艾希', img:'http://img2.wikia.nocookie.net/__cb20140210200942/leagueoflegends/images/4/4a/AsheSquare.png',text:''},
-  {name: '星朵拉', img:'http://img4.wikia.nocookie.net/__cb20120828063438/leagueoflegends/images/6/65/SyndraSquare.png',text:''},
-  {name: '阿璃', img:'http://cfile25.uf.tistory.com/image/237C1B48533A6FA00E7C0B',text:''},
+    text:'這是誰'},
+      {name: '安妮', img:'http://forums.euw.leagueoflegends.com/board/attachment.php?attachmentid=159092&d=1386868587',text:'<img>'},
+      {name: '拉克絲', img:'https://encrypted-tbn3.gstatic.com/images?q=tbn%3AANd9GcSQK2Its9rpYYaTx2Iks5nmz8-Np0V7HCAgwMJrTzQ-JnH0OcGlLw',text:''},
+      {name: '維嘉', img:'https://encrypted-tbn3.gstatic.com/images?q=tbn%3AANd9GcRT6sZ0SUWdnwyLaAVShflhx1RXUjY05vltg06oNSujF-KvKsTp',text:''},
+      {name: '犽宿', img:'http://tecnoslave.com/wp-content/uploads/2013/12/Yasuo_Square_0.jpg',text:''},
+      {name: '菲歐拉', img:'http://i.gbc.tw/2010/zone/lol/champion/120/fiora.png',text:''},
+      {name: '麗珊卓', img:'http://i.gbc.tw/2010/zone/lol/champion/120/lissandra.png',text:''},
+      {name: '阿姆姆', img:'http://i.gbc.tw/2010/zone/lol/champion/120/amumu.png',text:''},
+      {name: '布朗姆', img:'http://img.legames.cn/templates/images/upload/2014/07/1405329974_0.png',text:''},
+      {name: '易大師', img:'http://pic2.52pk.com/files/allimg/140724/2D0S4_203HUE6.png',text:''},
+      {name: '奈德麗', img:'http://tw.loldb.gameguyz.com//images/sqlite_images/champions_avatar_104_104/76.png',text:''},
+      {name: '威寇茲', img:'http://i.gbc.tw/2010/zone/lol/champion/120/velkoz.png',text:''},
+      {name: '雷茲', img:'http://i.gbc.tw/2010/zone/lol/champion/120/ryze.png',text:''},
+      {name: '艾希', img:'http://img2.wikia.nocookie.net/__cb20140210200942/leagueoflegends/images/4/4a/AsheSquare.png',text:''},
+      {name: '星朵拉', img:'http://img4.wikia.nocookie.net/__cb20120828063438/leagueoflegends/images/6/65/SyndraSquare.png',text:''},
+      {name: '阿璃', img:'http://cfile25.uf.tistory.com/image/237C1B48533A6FA00E7C0B',text:''},
+      {name: '厄薩斯', img:'https://lh4.googleusercontent.com/-jfwkpk77zBU/AAAAAAAAAAI/AAAAAAAAABE/RxfPUadm9sk/photo.jpg',text:''},
 
-  {name: '厄薩斯', img:'https://lh4.googleusercontent.com/-jfwkpk77zBU/AAAAAAAAAAI/AAAAAAAAABE/RxfPUadm9sk/photo.jpg',text:''},
+  ]
+    
+  })
 
-];
 }
